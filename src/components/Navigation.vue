07c70ab74f0546265b65efa0c6d869c0928a430d
Eddie-7 <85265500+Eddie-7@users.noreply.github.com>
2022-02-27 22:04:13 -0600

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -10,7 +10,7 @@
       <b-nav-item to="/lugares">El Salvador</b-nav-item>
       <b-nav-item to="/occidente">Zona Occidental</b-nav-item>
       <b-nav-item to="/oriente">Zona Oriental</b-nav-item>
-     
+     <b-nav-item to="/central">Zona Central</b-nav-item>
       
     </b-navbar-nav>
 
